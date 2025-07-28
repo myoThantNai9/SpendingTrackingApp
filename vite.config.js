c74dc068155a482e1991215bad2ca8a6cb6b6274
Myo Thant Naing <myothantnaing@Myos-MacBook-Pro.local>
2025-07-28 19:20:44 +0700

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/SpendingTrackingApp/', // ✅ match your GitHub repo name
-  
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
